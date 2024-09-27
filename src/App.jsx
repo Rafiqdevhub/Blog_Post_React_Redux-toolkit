2fdc4d65904f49d1fb7bad1e92d027deaa313c1d
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-27 11:22:30 +0500

    added post form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AddPostForm from "./components/AddPostForm";
+import PostsList from "./components/PostsList";
 
 const App = () => {
   return (
     <main className="App">
       <AddPostForm />
+      <PostsList />
     </main>
   );
 };
