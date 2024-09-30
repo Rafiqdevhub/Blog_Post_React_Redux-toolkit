35b83bc20060b3d1c49812518b34cde972452929
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-30 15:30:08 +0500

    added the functions to blog app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AddPostForm from "./components/AddPostForm";
 import Layout from "./components/Layout";
 import PostsList from "./components/PostsList";
 import SinglePostPage from "./components/SinglePostPage";
 import EditPostForm from "./components/EditPostForm";
+import UsersList from "./components/users/UsersList";
+import UserPage from "./components/users/UserPage";
 
 const App = () => {
   return (
@@ -16,6 +18,13 @@ const App = () => {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,23 +3,21 @@ import {
   selectAllPosts,
   getPostsStatus,
   getPostsError,
+  selectPostIds,
 } from "../features/posts/PostsSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
-  const posts = useSelector(selectAllPosts);
+  const orderedPostIds = useSelector(selectPostIds);
   const postStatus = useSelector(getPostsStatus);
   const error = useSelector(getPostsError);
 
   let content;
   if (postStatus === "loading") {
-    content = <p>&quot;Loading...&quot;</p>;
+    content = <p>"Loading..."</p>;
   } else if (postStatus === "succeeded") {
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date));
-    content = orderedPosts.map((post) => (
-      <PostsExcerpt key={post.id} post={post} />
+    content = orderedPostIds.map((postId) => (
+      <PostsExcerpt key={postId} postId={postId} />
     ));
   } else if (postStatus === "failed") {
     content = <p>{error}</p>;
